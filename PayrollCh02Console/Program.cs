d64adef9226ca24aa8e530fea78dfa755602325f
Jeff Scott <jpscott@ranken.edu>
2021-01-29 07:41:13 -0600

    Chapters 01 - 03 Payroll Program Commit Try #2

---
+++ PayrollCh02Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+/*
+ * 
+ *      A simple C# Console Payroll application.
+ * 
+ */
+
+namespace PayrollCh02Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Declare and initialize program variables
+            string firstName    = "";       //  Employee first name
+            string lastName     = "";       //  Employee last  name
+            double hoursWorked  = 0.0;      //  Employee hours worked
+            double hourlyRate   = 0.0;      //  Employee hourly rate
+            double grossPay     = 0.0;      //  hoursWorked * hourlyRate
+
+            //  Read in value for first name
+            Write("\n\n\t\tEnter Employee First Name: ");
+            firstName = ReadLine();
+
+            //  Read in value for last name
+            Write("\n\t\tEnter Employee  Last Name: ");
+            lastName = ReadLine();
+
+            //  Read in value for hours worked
+            Write("\n\t\tEnter Employee Hours Worked: ");
+            hoursWorked = Convert.ToDouble(ReadLine());
+
+            //  Read in value for hourly rate
+            Write("\n\t\tEnter Employee Hourly Rate: ");
+            hourlyRate = Convert.ToDouble(ReadLine());
+
+            //  Calculate gross pay
+            grossPay = hoursWorked * hourlyRate;
+
+            //  Output all input values and calculated gross pay
+            WriteLine("\n\n\t\tEmployee First  Name:  " + firstName);
+            WriteLine("\n\t\tEmployee Last   Name: " + lastName);
+            WriteLine("\n\t\tEmployee Hours  Work: " + hoursWorked.ToString("f2"));
+            WriteLine("\n\t\tEmployee Hourly Rate: " + hourlyRate.ToString("c"));
+            WriteLine("\n\t\tEmployee Gross   Pay: " + grossPay.ToString("c"));
+        }
+    }
+}
---
+++ PayrollCh03WindowsForms/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*
+ * 
+ *      A simple C# Payroll Windows Forms application.
+ * 
+ */
+ 
+namespace PayrollCh03WindowsForms
+{
+    public partial class FormPayrollChapter03 : Form
+    {
+        public FormPayrollChapter03()
+        {
+            InitializeComponent();
+        }
+
+        //  This method is run when the Calculate button
+        //  is clicked.  It grabs the values from the
+        //  hours worked and hourly rate textBoxes,
+        //  converts those values to their numeric 
+        //  equivalents, multiplies them and places the
+        //  answer in the formatted gross pay textBox.
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            //  Declare and initialize program variables
+            double hoursWorked = 0.0;
+            double hourlyRate = 0.0;
+            double grossPay = 0.0;
+
+            //  Convert hoursWorked to a double
+            hoursWorked = Convert.ToDouble(textBoxHoursWorked.Text);
+
+            //  Convert hourlyRate to a double
+            hourlyRate = Convert.ToDouble(textBoxHourlyRate.Text);
+
+            //  Calculate grossPay as hoursWorked * hourlyRate
+            grossPay = hoursWorked * hourlyRate;
+
+            //  Put gross pay into its formatted textBox
+            textBoxGrossPay.Text = grossPay.ToString("c");
+        }
+
+        //  This method is called when the Clear button
+        //  is clicked.  It clears out all textBoxes and
+        //  sets the cursor (focus) to the 1st textBox.
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxFirstName.Text   = "";
+            textBoxLastName.Text    = "";
+            textBoxHoursWorked.Text = "";
+            textBoxHourlyRate.Text  = "";
+            textBoxGrossPay.Text    = "";
+            textBoxFirstName.Focus();
+        }
+
+        //  This method is called when the Eixt button
+        //  is clicked.  It allows the user to end the
+        //  program if s/he so desires.
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show(
+                "Do You Really Want To Exit The Program?",
+                "EXIT NOW?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
